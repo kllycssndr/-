af87f1fd6af54e9be6aac044b7dedaeb89a9fafa
kllycssndr <31803635-kllycssndr@users.noreply.replit.com>
2024-02-03 02:50:37 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Kelly";
+    string lastName = "Paguiligan";
+    int age = 20;
+    Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
+    Console.WriteLine("Hello World" + {firstName} + " " + {lastName});
+  }
+}
